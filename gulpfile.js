680c8701772e9f66a8f2838d4164c1e433c1fca2
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-05-03 12:39:42 +0300

    Add project structure.
    Adds AUTHORS, LICENSE, package.json, .gitignore, gulp tasks.
    
    The library is now built to lib/qmlweb.parser.js and minified to
    lib/qmlweb.parser.min.js.
    
    Exports are now done through `module.exports` (for Node.js) and though
    `window.qmlweb_parse` (for browser, as before).
    
    PR-URL: https://github.com/qmlweb/qmlweb-parser/pull/6

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const changed = require('gulp-changed');
+const order = require('gulp-order');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const iife = require('gulp-iife');
+
+const sources = [
+  'src/**/*.js'
+];
+
+gulp.task('build-dev', function() {
+  return gulp.src(sources)
+             .pipe(order(sources, { base: __dirname }))
+             .pipe(sourcemaps.init())
+             .pipe(concat('qmlweb.parser.js'))
+             .pipe(iife({
+               useStrict: false,
+               params: ['exports'],
+               args: ['typeof exports !== \'undefined\' ? exports : window']
+             }))
+             .pipe(changed('./lib'))
+             .pipe(sourcemaps.write('./'))
+             .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('build', ['build-dev'], function() {
+  return gulp.src('./lib/qmlweb.parser.js')
+             .pipe(rename('qmlweb.parser.min.js'))
+             .pipe(changed('./lib'))
+             .pipe(sourcemaps.init({ loadMaps: true }))
+             .pipe(uglify())
+             .pipe(sourcemaps.write('./'))
+             .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(sources, ['build']);
+});
+
+gulp.task('watch-dev', ['build-dev'], function() {
+  gulp.watch(sources, ['build-dev']);
+});
+
+gulp.task('default', ['watch']);
